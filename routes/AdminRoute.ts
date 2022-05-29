7a58aa474762a3de06ba6bad1e2ec9174f3b8ca1
Amit Kawad <amitkawad100@gmail.com>
2022-05-29 15:06:22 +0530

    Added services structure

--- routes/AdminRoute.ts
+++ routes/AdminRoute.ts
@@ -1,9 +1,48 @@
-import express,{request,response,NextFunction, Router} from 'express';
+import express, { request, response, NextFunction, Router } from 'express';
 const router = express.Router();
+import { AdminService } from './../services/AdminService'
+//create an instance of AdminController
 
-router.get('/',(request,response)=>{
-    response.json({message: `Hello from admin route`});
-
+const adminService = new AdminService();
+router.get('/', (request, response) => {
+    response.json({ message: `Hello from admin route` });
 })
+const createVendor = async function () {
+    try {
+        const APIresponse = adminService.createVendor(request, response);
+        response.json(APIresponse);
+
+    } catch (error) {
+        throw error;
+
+    }
+
+}
+const getVendor = async function () {
+    try {
+        const APIresponse = adminService.getVendor(request, response);
+        response.json(APIresponse);
+
+    } catch (error) {
+        throw error;
+
+    }
+
+}
+const getVendorByid = async function(){
+    try {
+        const APIresponse = adminService.getVendorByid(request, response);
+        response.json(APIresponse);
+        
+    } catch (error) {
+        throw error;
+        
+    }
+}
+
+
+
+router.post('/vendor', createVendor);
+router.get('/vendor', getVendor);
 
 module.exports = router
---
+++ services/AdminService.ts
@@ -0,0 +1,15 @@
+export class AdminService {
+    createVendor = async function(request:any,response:any){
+        console.log('TEST');
+
+    }
+    getVendor = async function(request:any,response:any){
+    
+    }
+    getVendorByid = async function(request:any,response:any){
+    
+    }
+
+}
+
+
